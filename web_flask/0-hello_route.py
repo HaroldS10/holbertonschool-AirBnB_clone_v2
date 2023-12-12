37f503af975edab5d78440cef4475ee53736abd9
HaroldS10 <haroldsanty13@gmail.com>
2023-12-12 13:48:42 -0500

    host and port modifications2

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """
 The module creates a script that starts a Flask web application
 """
@@ -7,9 +6,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/", strict_slashes=False) # We use the route() decorator to tell Flask which URL should trigger our function.
+@app.route('/', strict_slashes=False) # We use the route() decorator to tell Flask which URL should trigger our function.
 def hello_world():
-    return ("Hello HBNB!")
+    return 'Hello HBNB!'
 
-if __name__ = '__main__':
-    app.run(host= '0.0.0.0', port= 5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port= 5000)
