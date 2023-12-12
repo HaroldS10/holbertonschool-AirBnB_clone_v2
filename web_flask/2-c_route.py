f18d3a9be1e0a500299bcf18a89e1f7c7532ee3a
HaroldS10 <haroldsanty13@gmail.com>
2023-12-12 15:53:36 -0500

    add _ character operation

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -11,18 +11,22 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 # We use route() decorator to tell Flask which URL should trigger our function
 def first_route():
     return'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def define_route():
     return'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def print_text(text):
-    return f'C {escape(text)}'
+    formatted_text = text.replace('_', ' ')
+    return f'C {escape(formatted_text)}'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
