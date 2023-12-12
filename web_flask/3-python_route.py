00c89ebb634f54a8ab8b5b65c42d1b863f558b08
HaroldS10 <haroldsanty13@gmail.com>
2023-12-12 16:29:24 -0500

    update NotText progress

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+
+"""
+The module creates a script that starts a Flask web application
+"""
+
+
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+# We use route() decorator to tell Flask which URL should trigger our function
+def first_route():
+    return'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def define_route():
+    return'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def print_text(text):
+    if not text:
+        return'is cool'
+    formatted_text = text.replace('_', ' ')
+    return f'C {escape(formatted_text)}'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
