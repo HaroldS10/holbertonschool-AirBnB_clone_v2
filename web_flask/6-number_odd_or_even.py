903937a51b1532d2408184fff8ba84d9b64d0a01
HaroldS10 <haroldsanty13@gmail.com>
2023-12-19 11:16:02 -0500

    sixth html file updated

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+
+
+"""Starts a Flask web application.
+
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
+    /c/<text>: Displays 'C' followed by the value of <text>.
+    /python/(<text>): Displays 'Python' followed by the value of <text>.
+    /number/<n>: Displays 'n is a number' only if <n> is an integer.
+    /number_template/<n>: Displays an HTML page only if <n> is an integer.
+    - Displays the value of <n> in the body.
+    /number_odd_or_even/<n>: Displays an HTML page only if <n> is an integer.
+        - States whether <n> is even or odd in the body.
+"""
+
+
+from flask import Flask
+from markupsafe import escape
+from flask import render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+# We use route() decorator to tell Flask which URL should trigger our function
+def first_route():
+    return'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def define_route():
+    return'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def print_text(text):
+    formatted_text = text.replace('_', ' ')
+    return f'C {escape(formatted_text)}'
+
+
+@app.route('/python/', defaults={'text': 'is_cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def print_text_py(text):
+    formatted_text = text.replace('_', ' ')
+    return f'Python {escape(formatted_text)}'
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    return "{} is a number".format(n)
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    return render_template("5-number.html", n=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def number_odd_or_even(n):
+    return render_template('6-number_odd_or_even.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
