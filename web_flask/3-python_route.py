f34d84aa246e2bdcaf80104ceecf7efa5f60fcf6
HaroldS10 <haroldsanty13@gmail.com>
2023-12-19 10:41:53 -0500

    adding module comments

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 
 
-"""
-The module creates a script that starts a Flask web application
+"""Starts a Flask web application.
+
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
+    /c/<text>: Displays 'C' followed by the value of <text>.
+    /python/(<text>): Displays 'Python' followed by the value of <text>.
 """
 
 
